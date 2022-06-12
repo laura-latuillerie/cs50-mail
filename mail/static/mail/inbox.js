659a4ce31381111896b4fa068f25c57133ca1013
laura-latuillerie <laura.latuillerie@gmail.com>
2022-06-12 17:22:36 +0200

    Change style + Improve and fix the loading of mailboxes + Comment js

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,19 +1,18 @@
 /***********************************************************************/
 //
-//
 document.addEventListener('DOMContentLoaded', function() {
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  // When you compose a new email and submit this form, it triggers the send email function
   document.querySelector('#compose-form').addEventListener('submit', send_email);
   // By default, load the inbox
   load_mailbox('inbox');
 });
 
 /***********************************************************************/
-//
 //
 function compose_email() {
   // Show compose view and hide other views
@@ -27,9 +26,8 @@ function compose_email() {
 
 /***********************************************************************/
 //
-//
 function send_email() {
-  // Get values from the forms
+  // Get email components' values from the form
   const recipients = document.querySelector('#compose-recipients').value;
   const subject = document.querySelector('#compose-subject').value;
   const body = document.querySelector('#compose-body').value;
@@ -43,17 +41,17 @@ function send_email() {
     })
   })
   .then(response => response.json())
-      .then(result => {
-        console.log(result);
-        // When successfully sent, load the "SENT" mailbox
-        load_mailbox('sent');
-      })
-      // Error prevention
-      .catch(error => {
-        console.log('Error:', error);
-    });
-    // Prevent default submission
-    return false;
+  .then(result => {
+    console.log(result);
+    // When successfully sent, load the "SENT" mailbox
+    load_mailbox('sent');
+  })
+  // Error prevention
+  .catch(error => {
+    console.log('Error:', error);
+  });
+  // Prevent default submission
+  return false;
 }
 
 /***********************************************************************/
@@ -69,32 +67,52 @@ function load_mailbox(mailbox) {
   document.querySelector('#mailbox-title').innerHTML = `<h3 class="text-center">${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
   // Use Api to fetch all the emails
   fetch(`/emails/${mailbox}`)
-    .then(response => response.json())
-    .then(emails => {
-        //
-        const email_table_sections = [['sender', 3], ['subject', 2], ['timestamp', 2], ['body', 5]];
-        const email_table_titles = {'sender': 'Sender', 'subject': 'Subject', 'timestamp': 'Date and time', 'body' : 'Content',  'read': true};
-        emails = [ email_table_titles, ...emails];
-        emails.forEach(email => {
-          const email_line = document.createElement('div');
-          email_line.classList.add("row","email-line", email["read"] ? "read" : "unread");
-            if (email ===  email_table_titles) {
-              email_line.id = 'email-table-titles';}
-            email_table_sections.forEach(
-                section => {
-                    const section_name = section[0];
-                    const section_size = section[1];
-                    const div_section = document.createElement('div');
-                    div_section.classList.add(`col-${section_size}`, `${section_name}-section`);
-                    div_section.innerHTML = `<p>${email[section_name]}</p>`;
-                    email_line.append(div_section);
-
-                            });
-            if (email !==  email_table_titles) {
-                email_line.addEventListener('click', () => load_email(email["id"], mailbox));
-            }
-
-            document.querySelector('#emails-view').append(email_line);
-        })
-})
-}
+  .then(response => response.json())
+  .then(emails => {
+    // Create objects for emails table
+    const email_table_sections = [['sender', 3], ['subject', 2], ['timestamp', 2], ['body', 5]];
+    // Re-assign titles for json fetched datas
+    const email_table_titles = {'sender': 'Sender', 'subject': 'Subject', 'timestamp': 'Date and time', 'body' : 'Content',  'read': true};
+    // Define the emails array
+    emails = [ email_table_titles, ...emails];
+    // Define an email line in the emails array => How your email will be represented and its composition
+    emails.forEach(email => {
+      // Create an HTML Div for all the "email lines"
+      const email_line = document.createElement('div');
+      // If this is the email line created for a titling purpose,
+      if (email ===  email_table_titles) {
+        // Add specific html details to it
+        email_line.classList.add("d-flex", "fw-bold");
+        email_line.id = 'email-table-titles';
+        // The condition below is my solution to prevent the titling row to repeat when toggling the nav buttons.
+        if (document.querySelector('#email-table-titles')) {
+          console.log('Already have titles')
+        } else {
+          document.querySelector('#email-table-titles-view').append(email_line);
+        }
+      }
+      // Create an email line :
+      email_table_sections.forEach(
+        // Define the composition of an email line thanks to the Objects created above
+        section => {
+          const section_name = section[0];
+          const section_size = section[1];
+          // Each email-line is composed of several sections. We create a div for each seaction and assign specific classes/content
+          const div_section = document.createElement('div');
+          div_section.classList.add(`col-${section_size}`, `${section_name}-section`, 'p-2', 'my-1', 'text-truncate');
+          div_section.innerHTML = `<p class="my-0">${email[section_name]}</p>`;
+          // Adding each section to create an email line.
+          email_line.append(div_section);
+        });
+        // For all the email lines except the titling one, 
+        if (email !==  email_table_titles) {
+          // Add specific classes
+          email_line.classList.add("row","email-line", "my-2", "px-1", "rounded", email["read"] ? "read" : "unread");
+          // Add the controls to open an individual view for each mail
+          email_line.addEventListener('click', () => load_email(email["id"], mailbox));
+          // All the email-lines will append and appear in the "emails view" div.
+          document.querySelector('#emails-view').append(email_line);
+        }
+      })
+    })
+  }
